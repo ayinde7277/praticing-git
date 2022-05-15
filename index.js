ce5d7679305b14baf675c00a4c003725725bb5ec
ayinde7277 <riliwanayinde40@gmail.com>
2022-05-15 06:17:51 -1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('praticing how to use git');
+function solution(number){
+    // convert the number to a roman numeral
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+  let roman='';
+  for (i in lookup){
+    while(number >= lookup[i]){
+      roman+=i;
+      number-=lookup[i];
+    }
+  }
+    return roman
+  }
